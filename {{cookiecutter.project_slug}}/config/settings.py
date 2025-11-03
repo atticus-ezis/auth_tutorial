eae9557a8a66de8e4a40e4dbd3e9e6034606b724
Atticus Ezis <atticus.ezis@gmail.com>
2025-11-03 22:36:33 +0900

    adjusted README

--- {{cookiecutter.project_slug}}/config/settings.py
+++ {{cookiecutter.project_slug}}/config/settings.py
@@ -14,8 +14,6 @@ import os
 from pathlib import Path
 from datetime import timedelta
 
-# Project directory name - used for URL configuration
-PROJECT_APP_NAME = "config"
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
